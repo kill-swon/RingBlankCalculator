2a853f80f32b2d63e1c5a0fe75f3d10eb54bb632
Morgan Espitia <morganespitia@gmail.com>
2025-02-11 18:23:25 -0700

    add react-native-paper and try metalThickness input.

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,26 @@
-import { Text, StyleSheet, View, useColorScheme } from "react-native";
+
+import React, { useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import { TextInput, Button } from "react-native-paper";
 import { StatusBar } from "expo-status-bar";
 
 export default function Index() {
-  const colorScheme = useColorScheme();
-
-  const themeTextStyle = colorScheme === 'light' ? styles.lightThemeText : styles.darkThemeText;
-  const themeContainerStyle = colorScheme === 'light' ? styles.lightContainer : styles.darkContainer;
+  const [metalThickness, setMetalThickness] = useState("");
 
   return (
-    <View style={[styles.container, themeContainerStyle]}>
-      <Text style={[styles.text, themeTextStyle]}>Color scheme: {colorScheme}</Text>
+    <View>
+      <TextInput
+        label="Metal Thickness (mm)"
+        mode="outlined"
+        keyboardType="numeric"
+        value={metalThickness}
+        onChangeText={setMetalThickness}
+      />
       <StatusBar />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text: {
-    fontSize: 20,
-  },
-  lightContainer: {
-    backgroundColor: '#d0d0c0',
-  },
-  darkContainer: {
-    backgroundColor: '#242c40',
-  },
-  lightThemeText: {
-    color: '#242c40',
-  },
-  darkThemeText: {
-    color: '#d0d0c0',
-  },
+
 });
